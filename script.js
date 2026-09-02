8f034465a5aa3e583496f1309cf8bb6d3c7adb67
Akula123sri <srikanthakula939@gmail.com>
2026-09-02 09:11:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,235 @@
+let age = 25;
+
+console.log("Declaration:", "let age");
+console.log("Initialization:", "age = 25");
+console.log("Age:", age);
+
+
+
+
+var a = 100;
+console.log(a);
+
+
+
+
+let number = 100;
+
+number = 200;
+
+console.log(number);
+
+
+
+var a = 10;
+
+console.log(a);
+
+
+
+
+var a = 10;
+
+a = 20;
+
+console.log(a);
+
+
+
+
+var a = 10;
+
+var a = 30;
+
+console.log(a);
+
+
+
+
+
+
+// var name = "John";
+
+// var price = 500;
+
+// price = 1000;
+
+// console.log("Name:", name);
+// console.log("Price:", price);
+
+
+
+var x = 50;
+
+x = 100;
+
+console.log(x);
+
+
+
+var x = 50;
+x = 100;
+
+console.log(x); // 100
+
+
+
+var x = 50;
+var x = 100;
+
+console.log(x);
+
+
+
+// let age = 25;
+
+// console.log(age);
+
+
+
+// let age = 20;
+// age = 30;
+// console.log(age);
+
+
+// let age = 20;
+// age = 30;
+
+// console.log(age);
+
+
+// let name = "John";
+// let name = "David";
+
+
+
+let city = "Chennai";
+
+city = "Salem";
+
+console.log(city);
+
+
+
+// let x = 10;
+// x = 50;
+// console.log(x);
+
+
+
+
+let salary = 25000;
+
+salary = 30000;
+
+console.log(salary);
+
+
+
+const pi = 3.14;
+
+console.log(pi);
+
+
+
+
+// const pi = 3.14;
+
+// pi = 3.14159;
+
+
+
+
+// const x = 10;
+// const x = 20;
+
+
+
+
+// const age;
+// age = 25;
+
+
+
+
+
+// const age = 25;
+
+// console.log(age);
+
+
+
+// const price = 500;
+
+// price = 1000;
+
+
+
+const country = "India";
+
+console.log(country);
+
+
+
+
+
+// const x = 100;
+
+// console.log(x);
+
+
+
+
+// let age = 20;
+// age = 25;
+
+// const pi = 3.14;
+// pi = 3.15; ❌ Error
+
+
+
+
+// var name = "John";
+// var name = "David";
+
+// console.log(name);
+
+
+
+console.log("Hello World");
+
+
+
+console.log(500);
+
+
+
+console.warn("Low Battery");
+
+
+
+
+console.error("Login Failed");
+
+
+
+console.log("Hello");
+
+
+
+alert("Welcome");
+
+
+
+let name = prompt("Enter your name");
+
+
+
+
+let result = confirm("Are you sure?");
+console.log(result);
+
+
+
+
+document.writeln("Hello World");
